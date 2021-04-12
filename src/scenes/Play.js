8e0ba248a080ab0bc7b5c54a335659da55ddcd8d
Fion Kwok <fikwok@ucsc.edu>
2021-04-11 20:54:01 -0700

    added controllable P1 rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,6 +1,6 @@
 
 
-class Play extends Phaser.Scene{
+class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
@@ -19,7 +19,7 @@ class Play extends Phaser.Scene{
         this.p1Rocket = new Rocket(
             this, 
             game.config.width/2,
-            game.config.height = borderUISize - borderPadding,
+            game.config.height - borderUISize - borderPadding,
             'rocket'
         );
 
@@ -32,14 +32,22 @@ class Play extends Phaser.Scene{
 
         // white borders
     	this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        //these rectangles won't spawn at the right location
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+      
+      
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
+
+        this.p1Rocket.update();
     }
 
 }
